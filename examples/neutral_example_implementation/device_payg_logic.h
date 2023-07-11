53013825ff0798f201adcb3f3ba08f5306bbd366
delyc <d.twizeyima@alustudent.com>
2023-07-11 09:27:18 +0200

    Feat: dded makefile

---
+++ examples/neutral_example_implementation/device_payg_logic.h
@@ -0,0 +1,22 @@
+#ifndef device_payg_logic_h
+#define device_payg_logic_h
+
+#if (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
+#include <sys/types.h>
+#endif
+
+#include "../include/extended/opaygo_core.h"
+#include "device_parameters.h"
+#include "unix_device/device_functions.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void UpdateDeviceStatusFromTokenValue(int TokenValue, int ActivationCount);
+void UpdateInvalidTokenWaitingPeriod(void);
+void SetTime(int NumberOfDaysToAdd);
+void AddTime(int NumberOfDaysToAdd);
+bool IsActive(void);
+bool TokenEntryAllowed(void);
+
+#endif
